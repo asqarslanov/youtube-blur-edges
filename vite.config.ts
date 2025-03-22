767a7207ad115c94392754d6b2f5079c8c0ad5d4
Asqar Arslanov <asqarslanov@gmail.com>
2025-03-22 19:50:28 +0300

    refactor: generate manifest in ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,14 @@
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
-import webExtension, { readJsonFile } from "vite-plugin-web-extension";
+import webExtension from "vite-plugin-web-extension";
 
-function generateManifest() {
-  const manifest = readJsonFile("src/manifest.json");
-  const pkg = readJsonFile("package.json");
-  return {
-    name: pkg.name,
-    description: pkg.description,
-    version: pkg.version,
-    ...manifest,
-  };
-}
+import * as manifest from "./manifest";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svelte(),
     webExtension({
-      manifest: generateManifest,
+      manifest: manifest.generate,
       watchFilePaths: ["package.json", "manifest.json"],
     }),
   ],
