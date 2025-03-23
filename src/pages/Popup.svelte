b1849ba13fbaa2cfe8dd0ff2e03fecbc05f5f57e
Asqar Arslanov <asqarslanov@gmail.com>
2025-03-23 14:18:06 +0300

    feat: set fixed bg size

--- src/pages/Popup.svelte
+++ src/pages/Popup.svelte
@@ -20,7 +20,10 @@
                 blurSize: number;
               };
 
-              function getBgVideo(mainVideo: HTMLVideoElement): HTMLDivElement {
+              function createBgVideo(
+                mainVideo: HTMLVideoElement,
+                config: Config,
+              ): HTMLDivElement {
                 const BACKGROUND_VIDEO_ID: string =
                   "youtube-blur-edges--background-video";
 
@@ -31,6 +34,14 @@
 
                 let bgVideo = document.createElement("div");
                 bgVideo.id = BACKGROUND_VIDEO_ID;
+
+                bgVideo.style.width = "100lvw";
+                bgVideo.style.height = "100lvh";
+                bgVideo.style.left = "0";
+                bgVideo.style.filter = `blur(${config.blurSize}px)`;
+                bgVideo.style.backgroundImage = `-moz-element(#${mainVideo.id})`;
+                bgVideo.style.backgroundSize = "cover";
+
                 mainVideo.parentNode!.insertBefore(bgVideo, mainVideo);
 
                 return bgVideo;
@@ -38,42 +49,18 @@
 
               const MAIN_VIDEO_ID: string = "youtube-blur-edges--main-video";
 
-              function onChange(
-                mainVideo: HTMLVideoElement,
-                config: Config,
-              ): void {
-                const bgVideo = getBgVideo(mainVideo);
-                bgVideo.style.left = "0";
-                bgVideo.style.width = "100%";
-                bgVideo.style.height = mainVideo.style.height;
+              const mainVideo = document.querySelector<HTMLVideoElement>(
+                "div#container > div.html5-video-player > div.html5-video-container > video.html5-main-video",
+              )!;
+              if (mainVideo.id.length === 0) {
+                mainVideo.id = MAIN_VIDEO_ID;
               }
 
-              function findMainVideo(): HTMLVideoElement | null {
-                return document.querySelector(
-                  "div#container > div.html5-video-player > div.html5-video-container > video.html5-main-video",
-                );
-              }
-
-              const mainVideo = findMainVideo()!;
-              mainVideo.id = MAIN_VIDEO_ID;
               const config: Config = {
-                blurSize: 16,
+                blurSize: 24,
               };
 
-              let bgVideo = getBgVideo(mainVideo);
-
-              bgVideo.style.filter = `blur(${config.blurSize}px)`;
-              bgVideo.style.backgroundImage = `-moz-element(#${MAIN_VIDEO_ID})`;
-              bgVideo.style.backgroundSize = "cover";
-
-              const observer = new MutationObserver(
-                (_mutationList, _observer) => {
-                  onChange(mainVideo, config);
-                },
-              );
-
-              onChange(mainVideo, config);
-              observer.observe(mainVideo, { attributes: true });
+              createBgVideo(mainVideo, config);
             },
             target: { tabId },
           });
